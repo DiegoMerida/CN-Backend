6c569cab188ad7cc0b9e851b068fd3840371192f
DiegoMerida <45089647+DiegoMerida@users.noreply.github.com>
2019-11-30 07:10:10 -0600

    configuration

---
+++ index.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+
+const { GraphQLServer } = require('graphql-yoga');
+const { importSchema } = require('graphql-import');
+const resolers = require('./src/resolvers');
+
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGO_URL,
+{
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+});
+
+const mongo = mongoose.connection;
+
+mongo.on('error', (error) => console.log(error))
+    .once('open', () => console.log('conected to database'));
+
+
+const typeDefs = importSchema( __dirname + '/schema.graphql');
+
+const server = new GraphQLServer({typeDefs, resolvers});
+
+server.start(()=> console.log('works! = u ='));
